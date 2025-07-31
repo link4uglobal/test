676b5f650a1af11c5379d15d918ab84b3940db0f
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2025-07-31 15:12:57 +0530

    add some

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -6,3 +6,16 @@ function w3_open() {
 function w3_close() {
     document.getElementById("mySidebar").style.display = "none";
 }
+// exper 
+  document.addEventListener("DOMContentLoaded", function () {
+    const stats = document.querySelectorAll(".stat-box");
+    stats.forEach((box, index) => {
+      box.style.opacity = 0;
+      box.style.transform = "translateY(20px)";
+      setTimeout(() => {
+        box.style.transition = "all 0.6s ease";
+        box.style.opacity = 1;
+        box.style.transform = "translateY(0)";
+      }, index * 200);
+    });
+  });
